f2100511e70dbf6126c3ab5caa6c7ad032f09960
Christopher Hertel <mail@christopher-hertel.de>
2024-06-23 19:32:08 +0200

    test: remove chat aware tests

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
-use App\Twig\ChatComponent;
 use PHPUnit\Framework\Attributes\CoversNothing;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpClient\TraceableHttpClient;
 use Symfony\UX\LiveComponent\Test\InteractsWithLiveComponents;
 
 #[CoversNothing]
@@ -23,31 +21,5 @@ final class SmokeTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertSelectorTextContains('.card-header strong', 'Chat with Symfony & GPT');
         self::assertSelectorExists('h4', 'Welcome to your Chat Bot with Symfony & GPT!');
-    }
-
-    public function testSubmitEndWithCorrectOrderOfMessages(): void
-    {
-        $client = static::createClient();
-        $component = $this->createLiveComponent(ChatComponent::class, [], $client);
-        $component->render();
-
-        $client->enableProfiler();
-        $component->call('submit', ['message' => 'How many days until Web Summer Camp starts?']);
-
-        self::assertResponseIsSuccessful();
-
-        /** @var TraceableHttpClient $tracedClient */
-        $tracedClient = self::getContainer()->get('TraceableHttpClient');
-        $requests = $tracedClient->getTracedRequests();
-
-        self::assertCount(1, $requests);
-        self::assertNotNull($requests[0]['options']['body'] ?? null);
-
-        $data = json_decode($requests[0]['options']['body'], true, 512, JSON_THROW_ON_ERROR);
-
-        self::assertCount(3, $data['messages']);
-        self::assertSame('system', $data['messages'][0]['role'], 'The first message should be the system prompt');
-        self::assertSame('assistant', $data['messages'][1]['role'], 'The second message should be the assistant prompt');
-        self::assertSame('user', $data['messages'][2]['role'], 'The third message should be the user prompt');
     }
 }
