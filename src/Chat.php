72eacb73059f02bc6e5ce4d2cbe139855b0733e5
Christopher Hertel <mail@christopher-hertel.de>
2024-07-05 16:41:56 +0200

    feat: switching to tool chain and implementing a clock tool

--- src/Chat.php
+++ src/Chat.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App;
 
 use App\OpenAI\GptClientInterface;
+use PhpLlm\LlmChain\Message\Message;
+use PhpLlm\LlmChain\Message\MessageBag;
+use PhpLlm\LlmChain\ToolChain;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 final class Chat
@@ -13,25 +16,24 @@ final class Chat
 
     public function __construct(
         private readonly RequestStack $requestStack,
-        private readonly GptClientInterface $gptClient,
+        private readonly ToolChain $toolChain,
     ) {
     }
 
-    /**
-     * @phpstan-return MessageList
-     */
-    public function loadMessages(): array
+    public function loadMessages(): MessageBag
     {
-        return $this->requestStack->getSession()->get(self::SESSION_KEY, []);
+        return $this->requestStack->getSession()->get(self::SESSION_KEY, new MessageBag());
     }
 
     public function submitMessage(string $message): void
     {
         $messages = $this->loadMessages();
 
-        $messages[] = ['role' => 'user', 'content' => $message];
-        $response = $this->gptClient->generateResponse($messages);
-        $messages[] = ['role' => 'assistant', 'content' => $response];
+        $message = Message::ofUser($message);
+        $response = $this->toolChain->call($message, $messages);
+
+        $messages[] = $message;
+        $messages[] = Message::ofAssistant($response);
 
         $this->saveMessages($messages);
     }
@@ -41,10 +43,7 @@ final class Chat
         $this->requestStack->getSession()->remove(self::SESSION_KEY);
     }
 
-    /**
-     * @phpstan-param MessageList $messages
-     */
-    private function saveMessages(array $messages): void
+    private function saveMessages(MessageBag $messages): void
     {
         $this->requestStack->getSession()->set(self::SESSION_KEY, $messages);
     }
